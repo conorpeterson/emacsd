422eab6cf1aa843544a87f5080793070545d99fc
Conor Peterson <cpeterson@meowwolf.com>
2017-08-15 22:36:16 -0600

    Initial commit

---
+++ init.el
@@ -0,0 +1,30 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+;;CUSTOM SECTION
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (misterioso)))
+ '(package-selected-packages (quote (arduino-mode nyan-mode rainbow-delimiters))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;MODULAR CONFIG
+(load "~/.emacs.d/c.el")
+;;(load "~/.emacs.d/themes/gray30-theme.el")
+(load "~/.emacs.d/org.el")
---
+++ org.el
@@ -0,0 +1,3 @@
+(add-hook 'org-mode-hook 'visual-line-mode)
+(add-hook 'org-mode-hook 'nyan-mode)
+
