0c95b6205af6cb0833c75d10235944e83ccf0332
Conor Peterson <cpeterson@meowwolf.com>
2017-10-14 10:00:06 -0600

    Decided on 4 spaces for a tab finally

--- python.el
+++ python.el
@@ -1,1 +1,6 @@
 (add-hook 'python-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'python-mode-hook
+      (lambda ()
+        (setq indent-tabs-mode nil)
+        (setq tab-width 4)
+        (setq python-indent 4)))
